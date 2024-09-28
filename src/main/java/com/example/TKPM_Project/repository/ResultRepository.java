8c8a3fd7d3693138252347ccacc3ff96a59e869b
NDKHANG21810212 <138880900+NDKHANG21810212@users.noreply.github.com>
2024-09-28 21:34:26 +0700

    fix ResultRepository

--- src/main/java/com/example/TKPM_Project/repository/ResultRepository.java
+++ src/main/java/com/example/TKPM_Project/repository/ResultRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ResultRepository extends JpaRepository<Result, Integer> {
-    // Ví dụ: lấy kết quả theo người dùng và bài thi
-    List<Result> findByUserIdAndExamId(Integer userId, Integer examId);
+
 }
